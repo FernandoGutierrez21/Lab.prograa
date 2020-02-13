7a98e510cd7002f0cf5a3579db1caa5d2ac5bdfc
FernandoGutierrez21 <60494918+FernandoGutierrez21@users.noreply.github.com>
2020-02-12 19:14:21 -0600

    practica1
    Gutierrez Santoyo

---
+++ Practica1.cpp
@@ -0,0 +1,16 @@
+/*Practica #1
+1917844
+30-01-2020*/
+
+#include"stdio.h"
+#include"conio.h"
+int main()
+{
+	//Salida de datos
+	printf("HOLA MUNDO. 1917844");
+	printf("\nHola a todo.\n");
+	
+
+	getche();
+	return 0;
+}
